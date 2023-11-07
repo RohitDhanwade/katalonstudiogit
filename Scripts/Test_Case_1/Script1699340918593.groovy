976b510497e38619b78c71b0d4e685a0fafa569b
A <A@DS009.step.com>
2023-11-07 12:48:40 +0530

    commi

--- Scripts/Test_Case_1/Script1699340918593.groovy
+++ Scripts/Test_Case_1/Script1699340918593.groovy
@@ -17,4 +17,7 @@ import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
-println("start process")
+println('start process')
+
+println('start process')
+
